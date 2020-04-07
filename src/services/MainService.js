4c86fa78bd70886eb6fc7b2025a533d8ced5eb60
vinayakgupta11 <vinayakgupta1107@gmail.com>
2020-04-07 20:25:48 +0530

    final code

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable comma-dangle */
 /* eslint-disable no-console */
@@ -6,17 +7,31 @@
 app.service('loginService', function () {
   // const admin = 'capgemini@gmail.com';
   // const pass = 'Capgemini123#';
-  const obj = {
+  const obj = [{
     Email: 'capgemini@gmail.com',
     Pass: 'Capgemini123#',
     Username: 'Capgemini',
     Contact: '1234567891'
-  };
+  },
+  {
+    Email: 'vinayak@gmail.com',
+    Pass: 'Vinayak123#',
+    Username: 'Vinayak',
+    Contact: '8567909036'
+  },
+  {
+    Email: 'developer@gmail.com',
+    Pass: 'Developer123#',
+    Username: 'Developer',
+    Contact: '1234567745'
+  }];
   this.login = function (InputData) {
-    if (InputData.email === obj.Email && InputData.password === obj.Pass) {
-      console.log('inputData', obj);
-      localStorage.setItem('UserData', JSON.stringify(obj));
-      return true;
+    for (let i = 0; i < obj.length; i++) {
+      if (InputData.email === obj[i].Email && InputData.password === obj[i].Pass) {
+        console.log('inputData', obj[i]);
+        localStorage.setItem('UserData', JSON.stringify(obj[i]));
+        return true;
+      }
     }
     return false;
   };
